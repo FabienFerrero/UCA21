9db395bd457785409561773a1d3dd7cf334cc4c1
Fabien Ferrero <32847282+FabienFerrero@users.noreply.github.com>
2022-02-02 18:09:45 +0100

    oled

---
+++ Libraries/lcdgfx/src/lcd_hal/avr/arduino/Arduino.h
@@ -0,0 +1,12 @@
+#ifndef _ARDUINO_H_
+#define _ARDUINO_H_
+
+#define ARDUINO_FAKE
+#include "../../avr/io.h"
+#include "../../Print_internal.h"
+
+#ifndef boolean
+typedef uint8_t boolean;
+#endif
+
+#endif
