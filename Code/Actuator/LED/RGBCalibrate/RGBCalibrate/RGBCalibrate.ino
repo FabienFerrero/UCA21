2f1f4c58772d8de04c8672d5f4422a4569389dff
FabienFerrero <fabien.ferrero@gmail.com>
2023-03-03 11:46:45 +0100

    Update RGBCalibrate.ino

--- Code/Actuator/LED/RGBCalibrate/RGBCalibrate/RGBCalibrate.ino
+++ Code/Actuator/LED/RGBCalibrate/RGBCalibrate/RGBCalibrate.ino
@@ -29,7 +29,7 @@
 //#define CLOCK_PIN 8
 
 CRGB leds[NUM_LEDS];
-int lum = 20;
+int lum = 21;
 
 void setup() {
     // sanity check delay - allows reprogramming if accidently blowing power w/leds
@@ -56,7 +56,7 @@ void loop() {
     leds[14] = CRGB(30,10,0);
     
     FastLED.show();
-    delay(3000);
+    delay(2000);
     leds[0] = CRGB(lum,0,0);
     leds[1] = CRGB(0,lum,0);
     leds[2] = CRGB(0,lum,lum);
@@ -66,49 +66,9 @@ void loop() {
     leds[6] = CRGB(0,lum,0);
     leds[7] = CRGB(0,0,lum);
     leds[8] = CRGB(0,lum,lum);
+    leds[10] = CRGB(0,0,50);
+    leds[12] = CRGB(30,0,0);
+    leds[14] = CRGB(0,20,0);
     FastLED.show();
-    do_sleep(5);
-        
-}
-
-void do_sleep(unsigned int sleepyTime) {
-  unsigned int eights = sleepyTime / 8;
-  unsigned int fours = (sleepyTime % 8) / 4;
-  unsigned int twos = ((sleepyTime % 8) % 4) / 2;
-  unsigned int ones = ((sleepyTime % 8) % 4) % 2;
-
-#ifdef SHOW_DEBUGINFO
-  Serial.print("Sleeping for ");
-  Serial.print(sleepyTime);
-  Serial.println(" seconds");  
-  delay(100); //Wait for serial to complete
-#endif
-
-
-  for ( int x = 0; x < eights; x++) {
-    // put the processor to sleep for 8 seconds
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  }
-  for ( int x = 0; x < fours; x++) {
-    // put the processor to sleep for 4 seconds
-    LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
-  }
-  for ( int x = 0; x < twos; x++) {
-    // put the processor to sleep for 2 seconds
-    LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
-  }
-  for ( int x = 0; x < ones; x++) {
-    // put the processor to sleep for 1 seconds
-    LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
-  }
-  addMillis(sleepyTime * 1000);
-}
-
-extern volatile unsigned long timer0_millis;
-void addMillis(unsigned long extra_millis) {
-  uint8_t oldSREG = SREG;
-  cli();
-  timer0_millis += extra_millis;
-  SREG = oldSREG;
-  sei();
+    delay(2000);        
 }
